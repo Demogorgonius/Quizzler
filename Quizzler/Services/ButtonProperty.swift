1f16fbb5dcefbb8f53624a54e7e58d4b71ae6bdb
Zvontsov Sergey <zvoncov@gmail.com>
2023-01-26 11:59:36 +0300

    Add buttons property

---
+++ Quizzler/Services/ButtonProperty.swift
@@ -0,0 +1,14 @@
+//
+//  ButtonProperty.swift
+//  Quizzler
+//
+//  Created by Sergey on 26.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct ButtonProperty {
+    let buttonFrame: CGRect = CGRect(x: 0, y: 0, width: 374, height: 80)
+    let buttonFont: UIFont = UIFont.systemFont(ofSize: 25.0, weight: .regular)
+}
