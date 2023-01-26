832c977a153e46bfcde2ceedb2cc550b774b5903
Zvontsov Sergey <zvoncov@gmail.com>
2023-01-26 12:58:02 +0300

    Add all UI elements

---
+++ Quizzler/Services/ImageViewProperty.swift
@@ -0,0 +1,14 @@
+//
+//  ImageViewProperty.swift
+//  Quizzler
+//
+//  Created by Sergey on 26.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct BackgroundImageViewProperty {
+    let frame: CGRect = CGRect(x: 0, y: 0, width: 414, height: 102)
+    let image: UIImage = #imageLiteral(resourceName: "Background-Bubbles")
+}
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -13,7 +13,48 @@ class ViewController: UIViewController {
     
 //MARK: - View/StackView
     
+    lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10.0
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        [questionLabel,
+         backgroundImageView].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        return stackView
+    }()
     
+    lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10.0
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        [ self.trueButton,
+          self.falseButton,
+          self.progressView].forEach {
+            stackView.addArrangedSubview($0)
+            
+        }
+        return stackView
+    }()
+    
+    lazy var progressView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 414, height: 40))
+        view.backgroundColor = .clear
+        view.alpha = 1.0
+        view.addSubview(progressBar)
+        return view
+    }()
+    
+    lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView(frame: BackgroundImageViewProperty().frame)
+        imageView.image = BackgroundImageViewProperty().image
+        imageView.addSubview(buttonsStackView)
+        return imageView
+    }()
     
 //MARK: - Buttons
     lazy var trueButton: UIButton = {
@@ -44,8 +85,21 @@ class ViewController: UIViewController {
         let label = UILabel(frame: LabelProperty().labelFrame)
         label.textColor = LabelProperty().labelTextColor
         label.font = LabelProperty().labelFont
+        label.numberOfLines = 0
+        label.text = "Question Text"
         
         return label
+    }()
+    
+//MARK: - Other UI elements
+    
+    lazy var progressBar: UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .bar)
+        progressView.center = view.center
+        progressView.setProgress(0.0, animated: true)
+        progressView.trackTintColor = .white
+        progressView.tintColor = #colorLiteral(red: 1, green: 0.4588235294, blue: 0.6588235294, alpha: 1)
+        return progressView
     }()
     
 //MARK: - ViewDidLoad
@@ -53,6 +107,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.1923559308, green: 0.2327082157, blue: 0.3624993563, alpha: 1)
+        view.addSubview(verticalStackView)
+        makeConstraints()
         
     }
     
@@ -64,6 +120,37 @@ class ViewController: UIViewController {
     @objc func falseButtonPressed(_ sender: UIButton!) {
         
     }
+    
+//MARK: - Add constraints
+    
+    func makeConstraints() {
+        
+        verticalStackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.height.equalToSuperview()
+            make.leading.equalToSuperview()
+        }
+        
+        buttonsStackView.snp.makeConstraints { make in
+            make.centerX.equalTo(backgroundImageView)
+            make.centerY.equalTo(backgroundImageView)
+            make.height.equalTo(backgroundImageView)
+            make.width.equalTo(backgroundImageView)
+        }
+        
+        progressBar.snp.makeConstraints { (make) in
+            make.centerX.equalTo(progressView)
+            make.centerY.equalTo(progressView)
+            make.leading.equalTo(progressView).offset(15)
+            make.height.equalTo(10)
+        }
+        
+    }
+    
+    
+    
+    
 
 
 }
