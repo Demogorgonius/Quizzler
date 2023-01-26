36904aad9a09827c3e9cc32ae52ffea6bc196b8a
Zvontsov Sergey <zvoncov@gmail.com>
2023-01-26 11:03:00 +0300

    Create label programmatically

---
+++ Quizzler/Services/LabelProperty.swift
@@ -0,0 +1,15 @@
+//
+//  LabelProperty.swift
+//  Quizzler
+//
+//  Created by Sergey on 26.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct LabelProperty {
+    let labelFrame: CGRect = CGRect(x: 0, y: 0, width: 374, height: 614)
+    let labelFont: UIFont = UIFont.systemFont(ofSize: 30.0, weight: .bold)
+    let labelTextColor: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+}
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -6,13 +6,38 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+//MARK: - Variables
+    
+//MARK: - View/StackView
+    
+//MARK: - Buttons
+    lazy var trueButton: UIButton = {
+        let button = UIButton(frame: <#T##CGRect#>)
+    }()
+    
+//MARK: - Labels
+    
+    lazy var questionLabel: UILabel = {
+        let label = UILabel(frame: LabelProperty().labelFrame)
+        label.textColor = LabelProperty().labelTextColor
+        label.font = LabelProperty().labelFont
+        
+        return label
+    }()
+    
+//MARK: - ViewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.1923559308, green: 0.2327082157, blue: 0.3624993563, alpha: 1)
+        
     }
+    
+//MARK: - Methods
+    
 
 
 }
